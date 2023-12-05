f65e76e8db642430f8f5f32c3c1eff58cbacf342
Josh Errickson <josh@errickson.net>
2023-12-05 10:54:49 -0500

    Adding data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+##' @title Polynomial data
+##' @format A data frame for using with polynomial
+##' \describe{
+##'   \item{a}{Random normal variable}
+##'   \item{b}{Draw from 1-100}
+##' }
+##' @source Made up
+"polydat"
---
+++ data-raw/polydat.R
@@ -0,0 +1,4 @@
+polydat <- data.frame(a = rnorm(10),
+                      b = sample(1:100, 10))
+
+use_data(polydat, overwrite = TRUE)
